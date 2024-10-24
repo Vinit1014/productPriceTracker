8c1222e154f0073d5905fa83aec23b6b6f522272
Vinit <vinits1014@gmail.com>
2024-10-24 19:13:28 +0530

    Made changes 2

--- frontend/src/components/ProductForm.tsx
+++ frontend/src/components/ProductForm.tsx
@@ -11,32 +11,30 @@ const ProductForm: React.FC<ProductFormProps> = ({ onAddProduct }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  useEffect(() => {
+  useEffect(()=>{
     console.log(url);
-  }, [url]);
+  },[url])
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
-
+    
     try {
       const response = await fetch(`${BASE_URL}/api/product`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url }),
       });
-
+      
       if (!response.ok) {
         throw new Error('Failed to fetch product details');
       }
-
+      
       const data = await response.json();
-      console.log('Product fetched from backend: ' + data.product);
-
+      console.log("Product fetched from backend "+data.product);
+      
       onAddProduct(data.product);
     } catch (error) {
-      setError('Enter a valid Flipkart product URL'); 
       console.error(error);
     } finally {
       setUrl('');
@@ -45,7 +43,6 @@ const ProductForm: React.FC<ProductFormProps> = ({ onAddProduct }) => {
   };
 
   return (
-    <>
     <form onSubmit={handleSubmit} className="mb-6">
       <div className="flex gap-2">
         <input
@@ -55,19 +52,16 @@ const ProductForm: React.FC<ProductFormProps> = ({ onAddProduct }) => {
           placeholder="Paste Flipkart product URL here"
           required
           className="flex-grow px-4 md:py-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+        />
         <button
           type="submit"
           disabled={loading}
           className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50"
-          >
+        >
           {loading ? 'Fetching...' : 'Fetch Details'}
         </button>
       </div>
-      
     </form>
-    {error && <p className="text-red-500 text-center mt-2">{error}</p>}
-    </>
   );
 };
 
