5021fcd20a9afbf5536eb80de2ff6fad28a17de8
Vinit <vinits1014@gmail.com>
2024-11-24 21:03:17 +0530

    Made dockerFile

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://productpricetracker.onrender.com";
+// export const BASE_URL = "https://productpricetracker.onrender.com";
+export const BASE_URL = "https://puppeteer-backend-pgvx.onrender.com";
