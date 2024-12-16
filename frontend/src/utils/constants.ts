c4b07b24e856243d1bdd381beb6934a7334edf31
Vinit <vinits1014@gmail.com>
2024-12-16 23:30:17 +0530

    changed a bit on frontend

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "https://productpricetracker.onrender.com";
 // export const BASE_URL = "https://puppeteer-backend-pgvx.onrender.com";
+export const BASE_URL = "https://productpricetracker-1.onrender.com";
