408c0c937041aced2ac9dddec4ee58a87ade1b7d
Vinit <vinits1014@gmail.com>
2024-12-17 10:05:31 +0530

    changed config

--- backend/src/config/dbConnect.ts
+++ backend/src/config/dbConnect.ts
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    // await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect('mongodb+srv://v1n1ts0010:lH1KmWhuLRy9pS5u@pricetracker.0av8v.mongodb.net/?retryWrites=true&w=majority&appName=priceTracker', {
     });
     console.log('MongoDB connected');
   } catch (error) {
